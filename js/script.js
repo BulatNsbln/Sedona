62931a6c0b1d740c2778e8e15eff6a7e243402ff
Bulat <wegwegwegw99@mail.ru>
2019-01-09 15:49:27 +0400

    change script

--- js/script.js
+++ js/script.js
@@ -9,3 +9,34 @@ function show(event) {
 }
 
 
+let resultParent = document.querySelector(".search-item-wrapper .result");
+let resultChildren = document.querySelector(".search-item-wrapper-children .result");
+
+let buttonPlusParent = document.querySelector(".search-item-wrapper .plus");
+let buttonMinusParent = document.querySelector(".search-item-wrapper .minus");
+
+let buttonPlusChild = document.querySelector(".search-item-wrapper-children .plus");
+let buttonMinusChild = document.querySelector(".search-item-wrapper-children .minus");
+
+buttonPlusParent.addEventListener("click", function (event) {
+    event.preventDefault();
+    resultParent.innerHTML++;
+});
+
+buttonMinusParent.addEventListener("click", function (event) {
+    event.preventDefault();
+    resultParent.innerHTML--;
+});
+
+buttonPlusChild.addEventListener("click", function (event) {
+    event.preventDefault();
+    resultChildren.innerHTML++;
+});
+
+buttonMinusChild.addEventListener("click", function (event) {
+    event.preventDefault();
+    resultChildren.innerHTML--;
+});
+
+
+
